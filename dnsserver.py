d597b3315380ee65d604e15b377fc98568fa19ca
Marwa Elali <marwaelali@gmail.com>
2022-04-19 23:52:55 -0400

    wip - parsing dig query

--- dnsserver.py
+++ dnsserver.py
@@ -29,40 +29,37 @@ class DNSServer:
         host_socket.close()
         return localhost_ip
 
-    def parse_dig_query(self, packet):
+    def parse_dig_query(self, data, addr):
         '''
         Function: parse_dig_query - parses incoming dig queries
         Params: packet - tuple of (data, address)
         Return: none
         '''
-        # parse packet
-        data = packet[0]
-        addr = packet[1]
-
         # parse dig query
         dig_query = dnslib.DNSRecord.parse(data)
         print('incoming:\n', dig_query)
 
         question_name = dig_query.q.qname
+        question_type = dig_query.q.qtype
         print(f'Question ID: {question_name}')
         message_id = dig_query.header.id
         print(f'Message ID: {message_id}')
 
         # create response
         response = dnslib.DNSRecord(
-            q=dig_query.q,
+            dnslib.DNSHeader(id=message_id, qr=1, aa=1, ra=1),
+            q=dnslib.DNSQuestion(dig_query.q.qname, dig_query.q.qtype, dig_query.q.qclass),
             a=dnslib.RR(
                 rdata=dnslib.A(addr[0])
             )
         )
-        print('question type:\n', response.q.qtype)
-        print('response:\n', response.a)
+        print('question:\n', response.q)
+        print('answer:\n', response.a)
         # if qtype == 1 it is an A record
-        # TODO send response to client
-        # if response.q.qtype == 1 and response.q.qname == self.NAME:
-        #     print('Sending response to client')
-        #     print(dnslib.DNSRecord.parse(response.pack()))
-        #     self.udp_socket.sendto(response.pack(), addr)
+        if question_type == 1 and str(question_name)[:-1] == self.NAME:
+            print('Sending response to client')
+            # print(dnslib.DNSRecord.parse(response.pack()) == str(response))
+            self.udp_socket.sendto(response.pack(), addr)
 
     def run(self):
         '''
@@ -81,9 +78,9 @@ class DNSServer:
             sys.exit('Error binding socket')
 
         while True:
-            packet = self.udp_socket.recvfrom(self.BUFFER)
+            data, addr = self.udp_socket.recvfrom(self.BUFFER)
             # parse dig query
-            self.parse_dig_query(packet)
+            self.parse_dig_query(data, addr)
 
         # ? close socket - this won't get reached because of infinite loop
         # self.udp_socket.close()
